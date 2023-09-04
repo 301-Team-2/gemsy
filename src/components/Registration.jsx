664de0fbd2d8ab8b9b35b4912fd78ff0a2a9b45c
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-09-04 14:08:34 -0700

    added more components

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,56 +1,21 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Register() {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
+  const { loginWithRedirect } = useAuth0();
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleRegister = () => {
+    // Use Auth0's loginWithRedirect method to initiate the registration process.
+    loginWithRedirect();
   };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('/api/register', formData);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };  
 
   return (
     <div>
       <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={formData.username}
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        <button type="submit">Register</button>
-      </form>
+      <p>Click the button below to register using Auth0</p>
+      <button onClick={handleRegister}>Register with Auth0</button>
     </div>
   );
-};
+}
 
 export default Register;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+      >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
+
