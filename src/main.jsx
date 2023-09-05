cd4737517aa951b581361b7847dbaaa32e00e2af
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-09-05 10:43:19 -0700

    added css styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './app.css'
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 
