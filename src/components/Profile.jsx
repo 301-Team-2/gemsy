2b3d72b03783431d55da8d8250f8344502234036
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-09-05 15:38:54 -0700

    able to get data from backend

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,19 @@
+import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
+import { useLocationContext } from "./LocationContext"; // Use relative import
+
 
 function Profile() {
   const { user, isAuthenticated } = useAuth0();
+  const { savedLocations, removeLocation } = useLocationContext();
 
   if (!isAuthenticated) {
     return <div>Please log in to view your profile.</div>;
   }
+
+  const handleRemoveLocation = (index) => {
+    removeLocation(index); // Use the removeLocation function from the context
+  };
 
   if (!user) {
     return <div>Loading...</div>;
@@ -17,6 +25,16 @@ function Profile() {
       <img src={user.picture} alt={user.name} />
       <p>Name: {user.name}</p>
       <p>Email: {user.email}</p>
+
+      <h3>Saved Locations</h3>
+      <ul>
+        {savedLocations.map((location, index) => (
+          <li key={index}>
+            {location}
+            <button onClick={() => handleRemoveLocation(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
