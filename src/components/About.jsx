9a1db828f61381509f17654b918bb9c8573478ca
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-09-07 22:33:21 -0700

    pathing issues with img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const teamMembers = [
   },
   {
     name: 'Joshua Shea',
-    imageSrc: `public/alejandra.png`,
+    imageSrc: './assets/josh.png',
     githubLink: 'https://github.com/jshea44',
     linkedinLink: 'https://www.linkedin.com/in/joshshea44/',
     bio: [
